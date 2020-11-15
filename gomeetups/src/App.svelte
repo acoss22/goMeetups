13f6c3f6dcfcfc027224cf674da649d3265a946f
Sequeira <asequeir@PMINTL.NET>
2020-11-15 16:08:26 +0000

    header component

--- gomeetups/src/App.svelte
+++ gomeetups/src/App.svelte
@@ -1,30 +1,7 @@
 <script>
-
+	import Header from "./UI/Header.svelte";
 </script>
 
-<style>
-	header{
-		position: fixed;
-		width: 100%;
-		top:0;
-		left:0;
-		height: 4rem;
-		background: #cf0056 ;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		box-shadow: 0 2px 6px rgba(0,0,0,0.26);
-	}
 
-	h1{
-		color: #fff;
-		font-family: 'Roboto Slab', serif;
-		
-
-	}
-</style>
-
-<header>
-<h1>goMeetups</h1>	
-</header>
+<Header />
 
