331d85bf701847c76421b47d11f2a1851ac618cc
Sequeira <asequeir@PMINTL.NET>
2020-11-15 17:17:42 +0000

    form created and grid component

--- gomeetups/src/Meetups/MeetUpItem.svelte
+++ gomeetups/src/Meetups/MeetUpItem.svelte
@@ -1,71 +1,79 @@
+<script>
+    export let title;
+    export let subtitle;
+    export let imageUrl;
+    export let address;
+    export let description;
+    export let email;
+</script>
 
 <style>
     article {
-      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-      border-radius: 5px;
-      background: white;
-      margin: 1rem;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+        border-radius: 5px;
+        background: white;
+        margin: 1rem;
     }
-  
+
     header,
     .content,
     footer {
-      padding: 1rem;
+        padding: 1rem;
     }
-  
+
     .image {
-      width: 100%;
-      height: 14rem;
+        width: 100%;
+        height: 14rem;
     }
-  
+
     .image img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
     }
-  
+
     h1 {
-      font-size: 1.25rem;
-      margin: 0.5rem 0;
-      font-family: "Roboto Slab", sans-serif;
+        font-size: 1.25rem;
+        margin: 0.5rem 0;
+        font-family: "Roboto Slab", sans-serif;
     }
-  
+
     h1.is-favorite {
-      background: #01a129;
-      color: white;
-      padding: 0 0.5rem;
-      border-radius: 5px;
+        background: #01a129;
+        color: white;
+        padding: 0 0.5rem;
+        border-radius: 5px;
     }
-  
+
     h2 {
-      font-size: 1rem;
-      color: #808080;
-      margin: 0.5rem 0;
+        font-size: 1rem;
+        color: #808080;
+        margin: 0.5rem 0;
     }
-  
+
     p {
-      font-size: 1.25rem;
-      margin: 0;
+        font-size: 1.25rem;
+        margin: 0;
     }
-  
+
     div {
-      text-align: right;
+        text-align: right;
     }
-  </style>
-  
+</style>
+
 <article>
     <header>
-        <h1></h1>
-        <h2></h2>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <p>{address}</p>
     </header>
- <div class="image">
-        <img src="" alt="">
-</div>
-<div class="content">
-    <p></p>
-</div>
-<footer>
-    <button>Show Details</button>
-    <button>Favourite</button>
-</footer>
+    <div class="image"><img src={imageUrl} alt={title} /></div>
+    <div class="content">
+        <p>{description}</p>
+    </div>
+    <footer>
+        <a href={email}>Contact</a>
+        <button>Show Details</button>
+        <button>Favourite</button>
+    </footer>
 </article>
---
+++ gomeetups/src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,23 @@
+<script>
+    import MeetUpItem from "./MeetUpItem.svelte";
+
+    export let meetups;
+</script>
+
+<style>
+    #meetups {
+        margin-top: 5rem;
+    }
+</style>
+
+<section id="meetups">
+    {#each meetups as meetup}
+        <MeetUpItem
+            title={meetup.title}
+            subtitle={meetup.subtitle}
+            description={meetup.description}
+            imageUrl={meetup.imageUrl}
+            email={meetup.contactEmail}
+            address={meetup.address} />
+    {/each}
+</section>
