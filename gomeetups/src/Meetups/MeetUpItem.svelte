a3a492862f78e299a34a1acb5aa5c93ae5104d03
Sequeira <asequeir@PMINTL.NET>
2020-11-16 15:26:48 +0000

    badge added when something is favorite and removed when is unfavorited

--- gomeetups/src/Meetups/MeetUpItem.svelte
+++ gomeetups/src/Meetups/MeetUpItem.svelte
@@ -1,89 +1,104 @@
 <script>
     import { createEventDispatcher } from "svelte";
-    import Button from "./../UI/Button.svelte";
-    
+    import Button from "../UI/Button.svelte";
+    import Badge from "../UI/Badge.svelte";
+  
     export let id;
     export let title;
     export let subtitle;
     export let imageUrl;
-    export let address;
     export let description;
-    export let email; 
+    export let address;
+    export let email;
     export let isFav;
-
-const dispatcher = createEventDispatcher();
-</script>
-
-<style>
+  
+    const dispatch = createEventDispatcher();
+  </script>
+  
+  <style>
     article {
-        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-        border-radius: 5px;
-        background: white;
-        margin: 1rem;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+      border-radius: 5px;
+      background: white;
+      margin: 1rem;
     }
-
+  
     header,
     .content,
     footer {
-        padding: 1rem;
+      padding: 1rem;
     }
-
+  
     .image {
-        width: 100%;
-        height: 14rem;
+      width: 100%;
+      height: 14rem;
     }
-
+  
     .image img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
     }
-
+  
     h1 {
-        font-size: 1.25rem;
-        margin: 0.5rem 0;
-        font-family: "Roboto Slab", sans-serif;
+      font-size: 1.25rem;
+      margin: 0.5rem 0;
+      font-family: "Roboto Slab", sans-serif;
     }
-
+  
     h1.is-favorite {
-        background: #01a129;
-        color: white;
-        padding: 0 0.5rem;
-        border-radius: 5px;
+      background: #01a129;
+      color: white;
+      padding: 0 0.5rem;
+      border-radius: 5px;
     }
-
+  
     h2 {
-        font-size: 1rem;
-        color: #808080;
-        margin: 0.5rem 0;
+      font-size: 1rem;
+      color: #808080;
+      margin: 0.5rem 0;
     }
-
+  
     p {
-        font-size: 1.25rem;
-        margin: 0;
+      font-size: 1.25rem;
+      margin: 0;
     }
-
+  
     div {
-        text-align: right;
+      text-align: right;
     }
+  
     .content {
-        height: 4rem;
+      height: 4rem;
     }
-</style>
-
-<article>
+  </style>
+  
+  <article>
     <header>
-        <h1>{title}</h1>
-        <h2>{subtitle}</h2>
-        <p>{address}</p>
+      <h1>
+        {title}
+        {#if isFav}
+          <Badge>FAVORITE</Badge>
+        {/if}
+      </h1>
+      <h2>{subtitle}</h2>
+      <p>{address}</p>
     </header>
-    <div class="image"><img src={imageUrl} alt={title} /></div>
+    <div class="image">
+      <img src={imageUrl} alt={title} />
+    </div>
     <div class="content">
-        <p>{description}</p>
+      <p>{description}</p>
     </div>
     <footer>
-        <Button href="mailto:{email}" caption="Contact" />
-        <Button mode="outline" type="button" caption="{isFav ? 'Unfavorite' : 'Favorite'}" on:click={() => dispatch('tooglefavorite', id)} />
-        <Button type="button" caption="Show Details" />
+      <Button href="mailto:{email}" caption="Contact" />
+      <Button
+        mode="outline"
+        color={isFav ? null : 'success'}
+        type="button"
+        caption={isFav ? 'Unfavorite' : 'Favorite'}
+        on:click={() => dispatch('togglefavorite', id)} />
+      <Button type="button" caption="Show Details" />
     </footer>
-</article>
+  </article>
+  
--- gomeetups/src/Meetups/MeetupGrid.svelte
+++ gomeetups/src/Meetups/MeetupGrid.svelte
@@ -1,26 +1,36 @@
 <script>
-    import MeetUpItem from "./MeetUpItem.svelte";
-
+    import MeetupItem from "./MeetupItem.svelte";
+  
     export let meetups;
-</script>
-
-<style>
-    #meetups {
-        margin-top: 5rem;
+  </script>
+  
+  <style>
+    section {
+      width: 100%;
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-gap: 1rem;
     }
-</style>
-
-<section id="meetups">
+  
+    @media (min-width: 768px) {
+      section {
+        grid-template-columns: repeat(2, 1fr);
+      }
+    }
+  </style>
+  
+  <section id="meetups">
     {#each meetups as meetup}
-        <MeetUpItem
-            id={meetup.id}
-            title={meetup.title}
-            subtitle={meetup.subtitle}
-            description={meetup.description}
-            imageUrl={meetup.imageUrl}
-            email={meetup.contactEmail}
-            address={meetup.address} 
-            isFav={meetup.isFavorite}
-            on:toogleFavorite />
+      <MeetupItem 
+        id={meetup.id}
+        title={meetup.title}
+        subtitle={meetup.subtitle}
+        description={meetup.description}
+        imageUrl={meetup.imageUrl}
+        email={meetup.contactEmail}
+        address={meetup.address}
+        isFav={meetup.isFavorite}
+        on:togglefavorite />
     {/each}
-</section>
+  </section>
+  
