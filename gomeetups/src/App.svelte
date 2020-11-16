6187a1e7eaf77131fe3d43da3709758471bdbeed
Sequeira <asequeir@PMINTL.NET>
2020-11-16 22:43:18 +0000

    custom store

--- gomeetups/src/App.svelte
+++ gomeetups/src/App.svelte
@@ -1,41 +1,17 @@
 <script>
+  import meetups from './Meetups/meetups-store';
   import Header from "./UI/Header.svelte";
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
   import TextInput from "./UI/TextInput.svelte";
   import Button from "./UI/Button.svelte";
   import EditMeetup from "./Meetups/EditMeetup.svelte";
 
-  let meetups = [
-    {
-      id: "m1",
-      title: "Coding Bootcamp",
-      subtitle: "Learn to code in 2 hours",
-      description:
-        "In this meetup, we will have some experts that teach you how to code!",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG",
-      address: "27th Nerd Road, 32523 New York",
-      contactEmail: "code@test.com",
-      isFavorite: false
-    },
-    {
-      id: "m2",
-      title: "Swim Together",
-      subtitle: "Let's go for some swimming",
-      description: "We will simply swim some rounds!",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg",
-      address: "27th Nerd Road, 32523 New York",
-      contactEmail: "swim@test.com",
-      isFavorite: false
-    }
-  ];
+  
 
   let editMode;
 
   function addMeetup(event) {
-    const newMeetup = {
-      id: Math.random().toString(),
+    const meetupData = {
       title: event.detail.title,
       subtitle: event.detail.subtitle,
       description: event.detail.description,
@@ -43,9 +19,7 @@
       contactEmail: event.detail.email,
       address: event.detail.address
     };
-
-    // meetups.push(newMeetup); // DOES NOT WORK!
-    meetups = [newMeetup, ...meetups];
+    meetups.addMeetup(meetupData)
     editMode = null;
   }
 
@@ -55,12 +29,7 @@
 
   function toggleFavorite(event) {
     const id = event.detail;
-    const updatedMeetup = { ...meetups.find(m => m.id === id) };
-    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
-    const meetupIndex = meetups.findIndex(m => m.id === id);
-    const updatedMeetups = [...meetups];
-    updatedMeetups[meetupIndex] = updatedMeetup;
-    meetups = updatedMeetups;
+    meetups.toggleFavorite(id);
   }
 </script>
 
@@ -83,5 +52,5 @@
   {#if editMode === 'add'}
     <EditMeetup on:save={addMeetup} on:cancel={cancelEdit} />
   {/if}
-  <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
+  <MeetupGrid meetups={$meetups} on:togglefavorite={toggleFavorite} />
 </main>
---
+++ gomeetups/src/Meetups/meetups-store.js
@@ -0,0 +1,53 @@
+import { writable } from 'svelte/store';
+
+const meetups = writable([
+  {
+    id: 'm1',
+    title: 'Coding Bootcamp',
+    subtitle: 'Learn to code in 2 hours',
+    description:
+      'In this meetup, we will have some experts that teach you how to code!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG',
+    address: '27th Nerd Road, 32523 New York',
+    contactEmail: 'code@test.com',
+    isFavorite: false
+  },
+  {
+    id: 'm2',
+    title: 'Swim Together',
+    subtitle: "Let's go for some swimming",
+    description: 'We will simply swim some rounds!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg',
+    address: '27th Nerd Road, 32523 New York',
+    contactEmail: 'swim@test.com',
+    isFavorite: false
+  }
+]);
+
+const customMeetupsStore = {
+  subscribe: meetups.subscribe,
+  addMeetup: meetupData => {
+    const newMeetup = {
+      ...meetupData,
+      id: Math.random().toString(),
+      isFavorite: false
+    };
+    meetups.update(items => {
+      return [newMeetup, ...items];
+    });
+  },
+  toggleFavorite: id => {
+    meetups.update(items => {
+      const updatedMeetup = { ...items.find(m => m.id === id) };
+      updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+      const meetupIndex = items.findIndex(m => m.id === id);
+      const updatedMeetups = [...items];
+      updatedMeetups[meetupIndex] = updatedMeetup;
+      return updatedMeetups;
+    });
+  }
+};
+
+export default customMeetupsStore;
