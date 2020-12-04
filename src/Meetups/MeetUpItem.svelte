d535d859a1009bf021aa472faa5e9c070e3f0f68
acoss22 <anacseq@gmail.com>
2020-12-04 09:48:48 +0000

    fix title

--- src/Meetups/MeetUpItem.svelte
+++ src/Meetups/MeetUpItem.svelte
@@ -11,7 +11,7 @@
   export let imageUrl;
   export let description;
   export let address;
-  export let email;
+  // export let email;
   export let isFav;
 
   let isLoading = false;
@@ -70,12 +70,12 @@
     font-family: "Roboto Slab", sans-serif;
   }
 
-  h1.is-favorite {
+  /* h1.is-favorite {
     background: #01a129;
     color: white;
     padding: 0 0.5rem;
     border-radius: 5px;
-  }
+  } */
 
   h2 {
     font-size: 1rem;
